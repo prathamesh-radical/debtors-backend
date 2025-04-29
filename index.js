fddac219fe3512a72086d3c78e941111eeefbf67
PiyushB2003 <piyushborkar95@gmail.com>
2025-04-29 18:47:33 +0530

    added port

--- index.js
+++ index.js
@@ -33,6 +33,6 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-app.listen(process.env.PORT || 4000, () => {
+app.listen(process.env.PORT || 4000, '0.0.0.0', () => {
     console.log(`App listening on port ${process.env.PORT || 3000}`);
 });
