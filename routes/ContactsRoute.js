c345f7cf2927f63a902d9d873ffffc06cd13215a
Prathamesh Nimje <prathmesh.n@radicalglobal.net>
2026-04-03 13:30:44 +0530

    bug fixed

--- routes/ContactsRoute.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { DeleteContact } from '../controllers/ContactController';
-
-const ContactsRoute = express.Router();
-
-ContactsRoute.delete('/delete-contact', DeleteContact);
-
-export default ContactsRoute;
