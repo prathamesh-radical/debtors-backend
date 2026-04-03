f2b442a77e6a8b64d3f7a6b9c7fc6d5b60004426
Prathamesh Nimje <prathmesh.n@radicalglobal.net>
2026-04-03 13:15:23 +0530

    bug fixed

--- routes/UpdateStatusRoute.js
+++ routes/UpdateStatusRoute.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { UpdateReport, UpdateUserStatus } from '../controllers/UpdateUserStatusController.js';
+import { DeleteContact, UpdateReport, UpdateUserStatus } from '../controllers/UpdateUserStatusController.js';
 
 const UpdateStatusRoute = express.Router();
 
 UpdateStatusRoute.put('/users/:id/status', UpdateUserStatus);
 UpdateStatusRoute.put('/update-report', UpdateReport);
+UpdateStatusRoute.delete("/delete-contact", DeleteContact);
 
 export default UpdateStatusRoute;
